b3f3cb223370acd025b1e5f890f1cddb985c59dc
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-08-12 00:31:13 +0800

    Add GET to reservationMain

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,13 @@
 const controller = {
     getIndex: function (req, res) {
         res.render('index');
+    },
+
+    getReservationMain: function (req, res) {
+        res.render('reservation-main');
     }
 }
+
+
 
 module.exports = controller;
--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,5 @@ const controller = require('../controllers/controller.js')
 const app = express.Router();
 
 app.get('/', controller.getIndex);
-
+app.get('/reservation-main', controller.getReservationMain);
 module.exports = app;
