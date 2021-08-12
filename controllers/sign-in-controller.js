1c2113c434c75ad3eb4d504bed36e4be762950a9
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-12 16:54:24 +0800

    Added temporary sign in page to simulate signed in employee

---
+++ controllers/sign-in-controller.js
@@ -0,0 +1,27 @@
+const db = require('../models/db.js');
+const Employee = require('../models/employee-model.js');
+
+const signInController = {
+
+    getSignIn: function (req, res) {
+        res.render('sign-in');
+    },
+
+    postSignIn: function (req, res) {
+
+        let employee = {
+            username: req.body.username,
+            password: req.body.password
+        }
+
+        db.findOne(Employee, employee, function(result) {
+            if(result) {
+                req.session.username = result.username;
+                req.session.employeeID = result._id;
+                res.redirect('/index');
+            }
+        });
+    }
+}
+
+module.exports = signInController;
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const controller = require('../controllers/controller.js');
+const signInController = require('../controllers/sign-in-controller.js')
 const reservationController = require('../controllers/reservation-controller.js')
 
 const app = express.Router();
 
-app.get('/', controller.getIndex);
+app.get('/', signInController.getSignIn);
+app.post('/sign-in', signInController.postSignIn);
+app.get('/index', controller.getIndex);
 app.get('/reservation-main', controller.getReservationMain);
 app.get('/reservation-add', controller.getReservationAdd);
+
 module.exports = app;
