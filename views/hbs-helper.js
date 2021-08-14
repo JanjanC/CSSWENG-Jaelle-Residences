9e25b16ecdc2e213faaa3ad01844a26b9e639ece
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-14 17:42:51 +0800

    Fixed date format

--- views/hbs-helper.js
+++ views/hbs-helper.js
@@ -4,5 +4,5 @@ hbs.registerHelper('formatDate', function(date) {
 	var month = (date.getMonth() + 1).toString().padStart(2, 0);
 	var day = date.getDate().toString().padStart(2, 0);
 	var year = date.getFullYear().toString();
-    return month + '/' + day + '/' + year;
+    return `${month}-${day}-${year}`;
 });
