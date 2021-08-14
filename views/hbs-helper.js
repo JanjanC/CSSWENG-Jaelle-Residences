bcecdf5326b8e73cb1add82fb3b4adff793b9f5b
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-14 17:09:25 +0800

    Added hbs helper

---
+++ views/hbs-helper.js
@@ -0,0 +1,8 @@
+const hbs = require(`hbs`);
+
+hbs.registerHelper('formatDate', function(date) {
+	var month = (date.getMonth() + 1).toString().padStart(2, 0);
+	var day = date.getDate().toString().padStart(2, 0);
+	var year = date.getFullYear().toString();
+    return month + '/' + day + '/' + year;
+});
