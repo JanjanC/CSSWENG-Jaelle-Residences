8bb0546abd0424b0e3bc56c01247d8a320c3b567
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-08-14 23:59:56 +0800

    remove getEditReservation from controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,10 +2,6 @@ const controller = {
     getIndex: function (req, res) {
         res.render('index', {today: new Date()});
     },
-
-    getEditReservation: function (req, res) {
-        res.render('reservation-edit');
-    }
 }
 
 module.exports = controller;
