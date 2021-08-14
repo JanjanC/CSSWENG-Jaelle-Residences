8e529bc4f6d21c4470e0041bc0249b43f838f78c
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-14 18:32:44 +0800

    Updated schema based on product backlog

--- models/guest-model.js
+++ models/guest-model.js
@@ -13,78 +13,30 @@ var GuestSchema = new mongoose.Schema ({
         required: true
     },
 
-    // the guest's sex
-    sex: {
-        type: String,
-        required: true
-    },
-
     // the guest's birthday
     birthdate: {
-        type: Date,
-        required: true
+        type: Date
     },
 
     // the guest's address
     address: {
-        type: String,
-        required: true
+        type: String
     },
 
     // the guest's contact number
     contact_number: {
-        type: String,
-        required: true
-    },
-
-    // the guest's emergency contact person
-    emergency_contact_person: {
-        type: String,
-        required: true
-    },
-
-    // the guest's emergency contact number
-    emergency_contact_number: {
-        type: String,
-        required: true
+        type: String
     },
 
     // the guest's company name
     company_name: {
-        type: String,
-        // required: true
+        type: String
     },
 
-    // the guest's company address
-    company_address: {
-        type: String,
-        // required: true
-    },
-
-    // the type of id the guest presented
-    id_type: {
-        type: String,
-        required: true
-    },
-
-    // the id number in the guest's id
-    id_number: {
-        type: String,
-        required: true
-    },
-
-    // the expiration date of the id presented
-    id_expiration: {
-        type: Date,
-        required: true
-    },
-
-    // the guest's remaining credit
-    credit: {
-        type: Number,
-        required: true,
-        default: 0
-    },
+    // the guest's occupation
+    occupation: {
+        type: String
+    }
 });
 
 module.exports = mongoose.model('Guest', GuestSchema);
