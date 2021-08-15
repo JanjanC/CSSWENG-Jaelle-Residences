b41a4bd915bad52d8b491022e1bcebf4a4c0344b
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-08-15 22:02:00 +0800

    printer test

--- main.js
+++ main.js
@@ -1,32 +1,62 @@
-//configure environment variables
-process.env.HOSTNAME = "localhost"
-process.env.PORT = "3000"
-process.env.DB_URL = "mongodb+srv://admin:ilovecssweng@jaelle-residences.ptlf1.mongodb.net/jaelle-residences-db?retryWrites=true&w=majority"
-
 //import the necessary modules
-const path = require('path');
-const electron = require('electron');
-const server = require('./server.js');
-
+const electron = require("electron");
 //retrieves the necessary attributes from electron
-const {app, BrowserWindow} = electron;
+const { app, BrowserWindow } = electron;
 
 let mainWindow;
+
 //create new window once electron finishes initialization
-app.on('ready', function() {
-    //create new window
-    mainWindow = new BrowserWindow({
-        title: "Jaelle Residences",
-        // icon: "",
-        autoHideMenuBar: true
+app.on("ready", function () {
+  //create new window
+  mainWindow = new BrowserWindow({
+    title: "Jaelle Residences",
+    show: false,
+    // icon: "",
+    autoHideMenuBar: true,
+  });
+
+  //opens the web application
+  mainWindow.loadFile(`index.html`);
+
+  mainWindow.webContents.once("dom-ready", () => {
+    print();
+  });
+
+  function print(copies = 3, delay = 1500) {
+    let counter = 1;
+    let printDataPassed = false;
+    _print();
+
+    //checks whether to print another copy or wait
+    //does nothing when the printer is printing the current copy, status === 1024
+    //if the printer is done printing, proceed to print next copy after a certain delay
+    //stops checking if all copies have been printed
+    let interval = setInterval(() => {
+      if (mainWindow.webContents.getPrinters()[4].status === 1024) {
+        printDataPassed = true;
+      } else if (
+        printDataPassed &&
+        mainWindow.webContents.getPrinters()[4].status === 0
+      ) {
+        setTimeout(_print, delay);
+        printDataPassed = false;
+        counter++;
+      }
+
+      if (counter >= copies) {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
+
+  function _print() {
+    mainWindow.webContents.print({
+      silent: true,
     });
+  }
 
-    //opens the web application
-    mainWindow.loadURL(`http://${process.env.HOSTNAME}:${process.env.PORT}/`);
-
-    //terminate the electron application on window close
-    mainWindow.on('closed', function() {
-        app.quit();
-    });
-
+  //terminate the electron application on window close
+  mainWindow.on("closed", function () {
+    app.quit();
+  });
 });
--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/controller.js')
-
-const app = express.Router();
-
-app.get('/', controller.getIndex);
-
-module.exports = app;
