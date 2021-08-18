ff181a05a035525f0323a14e4c0292b1369ad541
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-08-18 14:27:45 +0800

    added preliminary algorithm for returning available rooms

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -42,6 +42,52 @@ const bookingController = {
                 res.send(result);
             }
         });
+    },
+
+    availableRooms: function(req, res){
+        // extract dates and room type
+        start = new Date("8/18/21");
+        end = new Date("8/21/21");
+        type = "Studio Type"
+
+        // set conditions for the queries
+        room_query ={
+            room_type: type
+        };
+
+        booking_query = {
+            booked_type: type,
+            start_date: {$gte: start},
+            end_date: {$lte: end},
+            $or: [{confirmed_reservation: {$exists: false}},{confirmed_reservation: true}]
+        };
+
+        // find rooms of a specified type
+        db.findMany(Room, room_query, function(room_result){
+            if(room_result){
+                // find bookings for rooms of the specified type between start and end date inclusive
+                db.findMany(Booking, booking_query, function(booking_result){
+                    let rooms = [];
+                    // store room numbers in an array
+                    for(i = 0; i < room_result.length; i++){
+                        rooms.push(result[i].room_number);
+                    }
+
+                    // check if there are existing bookings for each room and
+                    // remove them from the array if bookings are found
+                    for(i = 0; i < rooms.length; i++){
+                        for(j = 0; j < booking_result.length; j++){
+                            if(booking_result[j].room_number == rooms[i]){
+                                rooms.splice(i,1);
+                                i--;
+                                break;
+                            }
+                        }
+                    }
+                    // Array rooms will contain the available rooms
+                });
+            }
+        });
     }
 }
 
