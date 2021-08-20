743fcf2168c51a508d350f03948dc6731fd1c27d
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-20 12:47:40 +0800

    Fixed conditionals

--- views/hbs-helper.js
+++ views/hbs-helper.js
@@ -52,11 +52,11 @@ hbs.registerHelper('addLeadingZeros', function(num) {
 });
 
 //Conditional that returns the content inside if the date parameter is later than the current date
-hbs.registerHelper('isPastDate', function(date, options) {
+hbs.registerHelper('isNotPastDate', function(date, options) {
     let today = new Date();
 	let todayString = `${today.getFullYear().toString()}-${(today.getMonth() + 1).toString().padStart(2, 0)}-${today.getDate().toString().padStart(2, 0)}`;
 
-    if (new Date(todayString) >= new Date(date)) {
+    if (new Date(date) >= new Date(todayString)) {
         return options.fn(this);
     } else {
         return options.inverse(this);
