caa6bdc57b1733100c3653a16b710dd3f8263598
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-22 02:43:51 +0800

    Added password hashing

--- controllers/sign-in-controller.js
+++ controllers/sign-in-controller.js
@@ -1,24 +1,31 @@
 const db = require('../models/db.js');
 const Employee = require('../models/employee-model.js');
 
+const bcrypt = require(`bcryptjs`);
+
 const signInController = {
 
     getSignIn: function (req, res) {
         res.render('sign-in');
     },
 
-    postSignIn: function (req, res) {
+    postSignIn: function(req, res) {
 
-        let employee = {
-            username: req.body.username,
-            password: req.body.password
-        }
+        let username = req.body.username;
+        let password = req.body.password;
 
-        db.findOne(Employee, employee, function(result) {
+        db.findOne(Employee, {username: username}, function(result) {
             if(result) {
-                req.session.username = result.username;
-                req.session.employeeID = result._id;
-                res.redirect('index');
+                bcrypt.compare(password, result.password, function(err, equal) {
+                    if (equal) {
+                        req.session.username = result.username;
+                        req.session.employeeID = result._id;
+
+                        res.redirect('/index');
+                    } else {
+                        // TODO: redirect to sign in failure page
+                    }
+                });
             } else {
                 res.redirect('/error');
             }
