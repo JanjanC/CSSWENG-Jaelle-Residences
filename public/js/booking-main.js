33333cfaaf4638464594ccd82209b48af6381298
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-08-23 19:46:23 +0800

    added new route for get Booking with time

--- public/js/booking-main.js
+++ public/js/booking-main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
 	$('#booking-sidebar').addClass('active');
+
+	$('#chosen-time').change(function (){
+		date = new Date($('#hidden-date').text());
+		console.log(date);
+		console.log($('#chosen-time').val().slice(0,2));
+		console.log($('#chosen-time').val().slice(-2));
+		$.get('/time/booking', {day: date.getDate(), month: date.getMonth(), year: date.getFullYear(), hours: $('#chosen-time').val().slice(0,2), minutes: $('#chosen-time').val().slice(-2)}, function (){});
+	});
 });
