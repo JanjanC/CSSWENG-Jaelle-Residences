c1e7cf6fe73e8bf696b0e72968a1d6cd67215ccd
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-24 01:55:13 +0800

    Update the booking start on reservation load

--- public/js/booking.js
+++ public/js/booking.js
@@ -88,9 +88,6 @@ function submitForm () {
 	let reservation = $('#reservation_select').val();
 	let roomID = $('#room-id').text();
 
-	console.log(reservation);
-	console.log(roomID);
-
 	if (reservation != '') {
 		$('#form-submit').attr('action', `/booking/${roomID}/confirm`);
 	}
@@ -105,8 +102,17 @@ function updateForm () {
 
 	$.get('/reservation', {reservationID: reservationID}, function(result) {
 		if (result) {
-			let endDate = new Date(result.end_date);
-			endDate = `${endDate.getFullYear().toString()}-${(endDate.getMonth() + 1).toString().padStart(2, 0)}-${endDate.getDate().toString().padStart(2, 0)}`;
+			let startDate = '';
+			if (result.start_date) {
+				startDate = new Date(result.start_date);
+				startDate = `${startDate.getFullYear().toString()}-${(startDate.getMonth() + 1).toString().padStart(2, 0)}-${startDate.getDate().toString().padStart(2, 0)}`;
+			}
+
+			let endDate = '';
+			if (result.end_date) {
+				endDate = new Date(result.end_date);
+				endDate = `${endDate.getFullYear().toString()}-${(endDate.getMonth() + 1).toString().padStart(2, 0)}-${endDate.getDate().toString().padStart(2, 0)}`;
+			}
 
 			let birthdate = '';
 			if (result.guest.birthdate) {
@@ -114,6 +120,7 @@ function updateForm () {
 				birthdate = `${birthdate.getFullYear().toString()}-${(birthdate.getMonth() + 1).toString().padStart(2, 0)}-${birthdate.getDate().toString().padStart(2, 0)}`;
 			}
 
+			$('#start-date').val(startDate);
 			$('#end-date').val(endDate);
 			$('#firstname').val(result.guest.first_name);
 			$('#lastname').val(result.guest.last_name);
