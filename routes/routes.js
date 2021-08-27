92630f6b23b1e20b4acb447b214f9af9be6afa98
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-08-27 21:37:45 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const controller = require('../controllers/controller.js');
+const errorController = require('../controllers/error-controller.js');
 const signInController = require('../controllers/sign-in-controller.js');
 const calendarController = require('../controllers/calendar-controller.js');
 const reservationController = require('../controllers/reservation-controller.js');
+const bookingController = require('../controllers/booking-controller.js');
 
 const app = express.Router();
 
 app.get('/', signInController.getSignIn);
+
 app.post('/sign-in', signInController.postSignIn);
+
 app.get('/sign-out', signInController.getSignOut);
-app.get('/index', controller.getIndex);
 
 app.get('/calendar/:year-:month', calendarController.getCalendar);
 
@@ -21,11 +23,24 @@ app.get('/:year-:month-:day/reservation/create', reservationController.getCreate
 app.post('/reservation/create', reservationController.postCreateReservation);
 
 app.get('/reservation/:bookingID/edit', reservationController.getEditReservation);
+
 app.post('/reservation/:bookingID/edit', reservationController.postEditReservation);
 
 app.post('/reservation/:bookingID/delete', reservationController.postDeleteReservation);
 
-app.get(`/error`, controller.getError);
-app.get(`/*`, controller.getError);
-app.post(`/*`, controller.getError);
+app.get('/:year-:month-:day/booking', bookingController.getBookingScreen);
+
+app.get('/:year-:month-:day/booking/:roomID/create', bookingController.getCreateBooking);
+
+app.post('/booking/:roomID/create', bookingController.postCreateBooking);
+
+app.get('/room/availability', bookingController.checkAvailability)
+
+app.get('/room', bookingController.getRoom)
+
+app.get(`/error`, errorController.getError);
+
+app.get(`/*`, errorController.getError);
+
+app.post(`/*`, errorController.getError);
 module.exports = app;
