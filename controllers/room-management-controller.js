73d0f42bad6ae58dad0716157cb4a18e8315d6fb
JanjanC <janjan_chua@dlsu.edu.ph>
2021-08-29 21:06:04 +0800

    Retrieve rooms and bookings from db

--- controllers/room-management-controller.js
+++ controllers/room-management-controller.js
@@ -1,9 +1,99 @@
+const db = require('../models/db.js');
+const Activity = require('../models/activity-model.js');
+const Booking = require('../models/booking-model.js');
+const Guest = require('../models/guest-model.js');
+const Room = require('../models/room-model.js');
+
 const roomManagementController = {
 
     getRoomManagement: function (req, res) {
-        res.render('room-management');
+
+        let today = new Date();
+		let dateString = `${today.getFullYear().toString()}-${(today.getMonth() + 1).toString().padStart(2, 0)}-${today.getDate().toString().padStart(2, 0)}`;
+		let timeString = `${today.getHours().toString().padStart(2, 0)}:${(today.getMinutes()).toString().padStart(2, 0)}:00`;
+
+		//there is a given time
+		if (req.query.time !== undefined) {
+			timeString = `${req.query.time}:00`;
+		}
+
+        let date = new Date(`${dateString} ${timeString}`);
+
+        //find all the rooms in the database
+		db.findMany(Room, {}, function (roomResult) {
+
+			if (roomResult) {
+
+				let list = [];
+				//transform the list of rooms into an object so that a booking may be later linked to a room
+				for (let i = 0; i < roomResult.length; i++) {
+					let room = {
+						room: roomResult[i],
+						booking: null
+					}
+					list.push(room);
+				}
+
+                let booking = {
+					//the current date is between the start date and end date of the booking, inclusive
+		            start_date: {$lte: date},
+		            end_date: {$gte: date},
+		            $or: [
+                        //booked
+						{confirmed_reservation: {$exists: false}},
+		            	{confirmed_reservation: true},
+                        //checked in
+                        {checked_in: true}
+					],
+					is_cancelled: false,
+		        };
+
+				db.findMany(Booking, booking, function (bookingResult) {
+		        	if (bookingResult) {
+						//loop through each booking
+						for (let i = 0; i < bookingResult.length; i++) {
+							//loop through each room
+							for (let j = 0; j < list.length; j++) {
+								//check if the room id of the booking matches the id of the room
+								if (list[j].room._id.toString() == bookingResult[i].room._id.toString()) {
+									//links the room to a booking
+									list[j].booking = bookingResult[i];
+									break;
+								}
+							}
+							//loop through each of the connected rooms in the booking
+							for(let k = 0; k < bookingResult[i].room.connected_rooms.length; k++) {
+								//loop through each room
+								for (let j = 0; j < list.length; j++) {
+									//check if the room id of the connected rooms in the booking matches the id of the room
+									if (list[j].room._id.toString() == bookingResult[i].room.connected_rooms[k].toString()) {
+										//links the room to a booking
+										list[j].booking = bookingResult[i];
+										break;
+									}
+								}
+							}
+						}
+
+						let values = {
+							username: req.session.username,
+							list: list,
+							date: dateString,
+							time: timeString
+						}
+
+						//loads the main booking page
+						res.render('room-management', values);
+		        	} else {
+						res.redirect('/error');
+					}
+		        }, 'room guest');
+			} else {
+				res.redirect('/error')
+			}
+		}, undefined, {room_number: 'asc'});
     }
-    
+
 }
 
 module.exports = roomManagementController;
