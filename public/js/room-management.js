c1f918d6bb2cbbf3e295445881bd8057b559b0e0
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-08-30 09:55:14 +0800

    Create room-management.js

---
+++ public/js/room-management.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('#booking-sidebar').addClass('active');
+
+	$('#chosen-time').change(function (){
+		chooseTime();
+	});
+});
+
+function chooseTime() {
+	let time = $('#chosen-time').val();
+	let href = `${location.protocol}//${location.host}${location.pathname}`;
+
+	window.location.replace(`${href}?time=${time}`);
+}
