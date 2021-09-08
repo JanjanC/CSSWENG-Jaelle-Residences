42465d4be11f225d18cb31ce9827c501ceeeb934
JanjanC <janjan_chua@dlsu.edu.ph>
2021-09-09 01:00:46 +0800

    Save pax and payment input

--- controllers/room-management-controller.js
+++ controllers/room-management-controller.js
@@ -202,7 +202,9 @@ const roomManagementController = {
 				startDate: new Date (),
                 endDate: new Date(`${req.body.end_date} 12:00:00`),
 				//check in the guest
-				checkedIn: true
+				checkedIn: true,
+                pax: req.body.room_pax,
+                payment: req.body.room_payment
             }
         }
 		//confirm the reservation, assign the guest to a room, and update the booking dates
