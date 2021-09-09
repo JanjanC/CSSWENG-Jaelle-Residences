53c7b2fb5e963cf2c2d4a382d196225752baf5f9
JanjanC <janjan_chua@dlsu.edu.ph>
2021-09-10 02:06:37 +0800

    Updated variable names

--- public/js/booking.js
+++ public/js/booking.js
@@ -458,7 +458,7 @@ function computeBalance () {
 function checkAvailability () {
 	let startDate = $('#start-date').val();
 	let endDate = $('#end-date').val();
-	let bookingid = $('#booking-id').text();
+	let bookingID = $('#booking-id').text();
 
 	if (startDate && endDate && endDate >= startDate) {
 		let rooms = [];
@@ -473,7 +473,7 @@ function checkAvailability () {
 			startDate: startDate,
 			endDate: endDate,
 			rooms: rooms,
-			bookingid: bookingid
+			bookingID: bookingID
 		}
 
 		$.get('/room/availability', information, function(result) {
