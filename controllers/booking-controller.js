2b5725346ac5dd453ee310f1224ec788f89d3b6b
JanjanC <janjan_chua@dlsu.edu.ph>
2021-09-10 18:06:07 +0800

    Added saving of transaction

--- controllers/booking-controller.js
+++ controllers/booking-controller.js
@@ -412,22 +412,55 @@ const bookingController = {
                 db.updateOne(Guest, {_id: bookingResult.guest}, guest, function(guestResult) {
                     if (guestResult) {
 
-                        let activity = {
-                            employee: req.session.employeeID,
-                            booking: bookingResult._id,
-                            activity_type: 'Modify Booking',
-                            timestamp: new Date()
-                        }
+						let transaction = {
+							$set: {
+								duration: req.body.duration,
+								averageRate: req.body.room_rate,
+								roomCost: req.body.room_initial_cost,
+								pax: req.body.room_pax,
+								pwdCount: req.body.room_pwd,
+								seniorCitizenCount: req.body.room_senior,
+								additionalPhpDiscount: {
+									reason: req.body.room_discount_reason_php,
+									amount: req.body.room_discount_php
+								},
+								additionalPercentDiscount: {
+									reason: req.body.room_discount_reason_php,
+									amount: req.body.room_discount_percent
+								},
+								totalDiscount: req.body.room_subtract,
+								extraCharges: req.body.room_extra,
+								totalCharges: req.body.room_total_extra,
+								netCost: req.body.room_net_cost,
+								payment: req.body.room_payment,
+								balance: req.body.room_balance
+							}
+						}
 
-                        //saves the action of the employee to an activity log
-                        db.insertOne(Activity, activity, function(activityResult) {
-                            if (activityResult) {
-                                // redirects to home screen after updating the booking
-                                res.redirect(`/${req.body.start_date}/booking/`);
-                            } else {
-                                res.redirect('/error');
-                            }
-                        });
+						db.updateOne(Transaction, {_id: bookingResult.transaction}, transaction, function(transactionResult) {
+						    if (transactionResult) {
+
+								let activity = {
+									employee: req.session.employeeID,
+									booking: bookingResult._id,
+									activity_type: 'Modify Booking',
+									timestamp: new Date()
+								}
+
+								//saves the action of the employee to an activity log
+								db.insertOne(Activity, activity, function(activityResult) {
+									if (activityResult) {
+										// redirects to home screen after updating the booking
+										res.redirect(`/${req.body.start_date}/booking/`);
+									} else {
+										res.redirect('/error');
+									}
+								});
+
+						    } else {
+						        res.redirect('/error');
+						    }
+						});
                     } else {
                         res.redirect('/error');
                     }
