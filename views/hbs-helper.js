5b079c08f9e732919b6cd524e265acfd7cad470c
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-09-16 11:05:54 +0800

    Added helper for status

--- views/hbs-helper.js
+++ views/hbs-helper.js
@@ -113,3 +113,10 @@ hbs.registerHelper('roundNumber', function(value) {
     }
 
 });
+
+hbs.registerHelper('ifStatus', function(v1, v2, options) {
+  if(v1 || v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
