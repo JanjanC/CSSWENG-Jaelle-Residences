461cc6eae63762f85214c2414d06047bf2a9d58a
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-09-16 16:57:34 +0800

    removed unnecessary lines of code

--- public/js/print.js
+++ public/js/print.js
@@ -4,12 +4,7 @@ const electron = require("electron");
 const {ipcRenderer} = electron;
 
 $(document).ready(function () {
-	$('#print-test').click(function() {
-        ipcRenderer.send('print:goto', "613f1c4f1f3b0e4ddc0cbcd3");
-    });
-
     $('#print-it').click(function() {
-        $('#print-test').hide();
         $('#print-it').hide();
         ipcRenderer.send('print:execute');
     });
