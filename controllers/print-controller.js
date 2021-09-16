67f0a67c34989ed6fa89e183f0d85666075dc7ec
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-09-16 19:15:11 +0800

    custom event emitter for print event

---
+++ controllers/print-controller.js
@@ -0,0 +1,17 @@
+const events = require("events");
+const printEvent = new events.EventEmitter();
+
+//the callback receives the argument 'data' from the emitter
+function onPrintEvent(callback) {
+  printEvent.on("print", (data) => {
+    console.log("PRINTEVENT OCCURRED");
+    callback(data);
+  });
+}
+
+//sends the parameter args to the receiver of 'print' event
+function emitPrintEvent(args) {
+  printEvent.emit("print", args);
+}
+
+module.exports = { onPrintEvent: onPrintEvent, emitPrintEvent: emitPrintEvent };
