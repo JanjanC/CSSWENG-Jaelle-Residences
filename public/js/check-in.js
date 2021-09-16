09b0c0636da6caa0999c48c52c0cd9464a18b666
ilearnonthisaccount <raynier_timothy_chua@dlsu.edu.ph>
2021-09-16 16:57:07 +0800

    implemented printing when checkbox is checked

--- public/js/check-in.js
+++ public/js/check-in.js
@@ -1,3 +1,6 @@
+const electron = require("electron");
+const {ipcRenderer} = electron;
+
 $(document).ready(function () {
 	computeInitialCost();
 	computeCharges();
@@ -143,6 +146,9 @@ function getRoomInfo () {
 function submitForm () {
 	if ($('#reservation_select').val()) {
 		$('#form-submit').attr('action', `/management/${$('#room-id').text()}/checkin/vacant/booking`);
+	}
+	if($('#print-receipt').is(":checked")){
+		ipcRenderer.send('print:goto', $('#booking-id').text());
 	}
 	return true;
 }
