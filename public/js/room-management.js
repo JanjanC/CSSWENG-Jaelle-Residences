05c540925441650adf78be074cb780b3d97a798d
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2021-09-19 16:55:06 +0800

    Added print button with functionality to room card (#140)

--- public/js/room-management.js
+++ public/js/room-management.js
@@ -1,8 +1,22 @@
+//import the necessary modules
+const electron = require("electron");
+//retrieves the necessary attributes from electron
+const {ipcRenderer} = electron;
+
 $(document).ready(function () {
 	$('#nav-room').addClass('active');
 
 	$('#chosen-time').change(function (){
 		chooseTime();
+	});
+
+	$("#print-link").on("click", function(e) {
+		e.preventDefault();
+
+		let href = $("#print-link").attr('href');
+		let bookingID = href.split('/')[2];
+		
+	    ipcRenderer.send('print:goto', bookingID);
 	});
 });
 
@@ -12,3 +26,4 @@ function chooseTime() {
 
 	window.location.replace(`${href}?time=${time}`);
 }
+
