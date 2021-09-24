8e11372104dc26bb5f64ee3830fe84dd31d59fb8
JanjanC <janjan_chua@dlsu.edu.ph>
2021-09-25 00:15:37 +0800

    Update the documentation for print-controller

--- controllers/print-controller.js
+++ controllers/print-controller.js
@@ -3,14 +3,16 @@ const printEvent = new events.EventEmitter();
 
 //the callback receives the argument 'data' from the emitter
 function onPrintEvent(callback) {
-  printEvent.on("print", (data) => {
-    callback(data);
-  });
+    //invoke the callback function on a print event
+    printEvent.on("print", (data) => {
+        callback(data);
+    });
 }
 
 //sends the parameter args to the receiver of 'print' event
 function emitPrintEvent(args) {
-  printEvent.emit("print", args);
+    //create a print event
+    printEvent.emit("print", args);
 }
 
 module.exports = { onPrintEvent: onPrintEvent, emitPrintEvent: emitPrintEvent };
